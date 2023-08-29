bdc5dd63228956684efeddbeab32d818f9632ed6
FedericoRaizman <127147566+FedericoRaizman@users.noreply.github.com>
2023-08-29 12:01:41 -0300

    Aro resuelto

--- www/js/script.js
+++ www/js/script.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const calendarContainer = document.getElementById("calendar");
     const eventForm = document.getElementById("eventForm");
   
-    // Objeto para almacenar los eventos
     let events = [];
   
-    // Función para agregar un evento al array de eventos
     function addEvent(eventName, eventDate) {
       events.push({ name: eventName, date: eventDate });
     }
 });
  
   
-  
+    
