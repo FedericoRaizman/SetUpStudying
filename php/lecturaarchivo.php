25be1565a0a152d276f0032cc75031fd7ff01b41
SofiRzo <sof.rzo2006@gmail.com>
2023-10-31 10:08:32 -0300

    borre lectura archivo

--- php/lecturaarchivo.php
+++ php/lecturaarchivo.php
@@ -1,11 +1,13 @@
 <?php
 
-$fp = fopen("Hola.docx", "r");
-
-if (!$fp = fopen("Hola.docx, "r")){
-
+//$fp = fopen("Hola.docx", "r");
+if (!$fp = fopen("../uploads/chau.txt", "r")){
     echo "No se ha podido abrir el archivo";
+}else{
+    while(!feof($fp)){
+        echo fread($fp, 4092);
+    }
+}
 
-}$fp = fopen(", "r");
 
 ?>
