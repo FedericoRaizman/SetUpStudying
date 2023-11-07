c6e3344293b3af42d02291b1410b77b852af790e
nicogoldzen <47393704@est.ort.edu.ar>
2023-11-07 09:01:54 -0300

    csstodo

--- html/script.js
+++ html/script.js
@@ -1,19 +1,4 @@
-const home = document.getElementById("home");
-const carpetas = document.getElementById("carpetas");
-const calendario = document.getElementById("calendario");
-const documentos = document.getElementById("documentos");
-
-
-uno.addEventListener("click", () => {
-camara.style.display = "block";
-info.style.display = "none";
-soporte.style.display = "none";
-primeraImpresion.style.display = "none";
-});
-
-infobtn.addEventListener("click", () => {
-camara.style.display = "none";
-info.style.display = "block";
-soporte.style.display = "none";
-primeraImpresion.style.display = "none";
+document.getElementById("menuButton").addEventListener("click", function() {
+    var containerMenu = document.querySelector(".container-menu");
+    containerMenu.classList.toggle("active");
 });
