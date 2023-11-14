81cc8c8bd8f0dc30cf9efcbf3c2e3f9433d396c5
FedericoRaizman <47436581@est.ort.edu.ar>
2023-11-14 18:46:38 -0300

    update

--- php/eventos.php
+++ php/eventos.php
@@ -3,17 +3,37 @@
 session_start();
 include("conexion.php");
 
-$sql = "SELECT * FROM eventos WHERE IDUSUARIO=".$_SESSION['Usu']."";
-//echo $sql;
-$datos = $conexion->query($sql);
 
-if($datos->num_rows>0){
-    while($row[]=$datos->fetch_assoc()){
-    }
+$accion= (isset($_GET['accion']))?$_GET['accion']:'leer';
+switch($accion){
+
+          case 'agregar' :
+
+            break;
+            case 'eliminar':
+
+                break;
+            case 'modificar':
+
+                break;
+        default:
+
+        $sql = "SELECT * FROM eventos WHERE IDUSUARIO=".$_SESSION['Usu']."";
+        //echo $sql;
+        $datos = $conexion->query($sql);
+        
+        if($datos->num_rows>0){
+            while($row[]=$datos->fetch_assoc()){
+            }
+        }
+        
+        echo json_encode($row);
+        $conexion->close();
+
+        break;
+
 }
 
-echo json_encode($row);
-$conexion->close();
 
 
 
